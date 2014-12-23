fda9c826e50a1f5ab6e6d2cf756f4bc1a9df7ae4
JD <test.machinery@gmail.com>
2014-12-23 10:46:37 +0100

    move the academic mining code here

---
+++ natureProductCodes.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+natureProductCodes = {
+	
+	# A serias
+	'Acta Pharmacologica Sinica' : 'aps',
+	'American Journal of Gastroenterology' : 'ajg',
+	'American Journal of Gastroenterology Supplements' : 'ajgsup'
+	'Asian Journal of Andrology' : 'aja'
+
+`	# B series
+	'Blood Cancer Journal' : 'bcj',
+	'Bone Marrow Transplantation' : 'bmt',
+	'BoneKEy Reports' : 'bonekeyreports'
+	'British Dental Journal' : 'bdj'
+	'British Journal of Cancer' : 'bjc',
+
+	# C series
+	'Cancer Gene Therapy' : 'cgt',
+	'Cell Death & Differentiation' : 'cdd',
+	'Cell Death & Disease' : 'cddis',
+	'Cell Research' : 'cr',
+	'Cellular & Molecular Immunology' : 'cmi',
+	'Clinical and Translational Gastroenterology' : 'ctg',
+	'Clinical Pharmacology & Therapeutics' : 'clpt',
+	'CPT: Pharmacometrics & Systems Pharmacology' : 'psp',
+	'Clinical & Translational Immunology' : 'cti',
+
+	# E series
+	'Emerging Microbes & Infections' : 'emi',
+	'European Journal of Clinical Nutrition' : 'ejcn',
+	'European Journal of Human Genetics' : 'ejhg',
+	'Evidence-Based Dentistry' : 'ebd',
+	'Experimental & Molecular Medicine' : 'emm',
+	'Eye' : 'eye',
+
+	# G
+	'Gene Therapy' : 'gt',
+	'Genes and Immunity' : 'gene',
+	'Genetics in Medicine' : 'gim',
+
+	'Heredity' : 'hdy',
+	'History of the journal Nature' : '',
+	'Horticulture Research' : 'hortres',
+	'Human Genome Variation' : 'hgv',
+	'Hypertension Research' : 'hr',
+
+	'IBMS BoneKEy' : 'bonekey',
+	'Immunology & Cell Biology' : 'icb',
+	'International Journal of Impotence Research' : 'ijir',
+	'International Journal of Obesity' : 'ijo',
+	'International Journal of Obesity Supplements' : 'ijosup',
+	'International Journal of Oral Science' : 'ijos',
+	'The ISME Journal' : 'ismej',
+
+	'The Journal of Antibiotics' : 'ja',
+	'Journal of Cerebral Blood Flow and Metabolism' : 'jcbfm',
+	'Journal of Exposure Science and Environmental Epidemiology' : 'jes',
+	'Journal of Human Genetics' : 'jhg',
+	'Journal of Human Hypertension' : 'jhh',
+	'The Journal of Investigative Dermatology' : 'jid',
+	'JID Symposium Proceedings' : 'jidsp',
+	'Journal of Perinatology' : 'jp',
+
+	'Kidney International' : 'ki',
+	'Kidney International Supplements' : 'kisup',
+
+	'Laboratory Investigation' : 'labinvest',
+	'Leukemia' : 'leu',
+	'Leukemia Supplements' : 'leusup',
+	'Light: Science & Applications' : 'lsa',
+
+	'Modern Pathology' : 'modpathol',
+	'Molecular Psychiatry' : 'mp',
+	'Molecular Therapy' : 'mt',
+	'Molecular Therapy — Methods & Clinical Development' : 'mtm',
+	'Molecular Therapy — Nucleic Acids' : 'mtna',
+	'Mucosal Immunology' : 'mi',
+
+	# N series
+	'Nature' : 'nature',
+	'Nature Arabic Edition' : '',
+	'Nature Biotechnology' : 'nbt',
+	'Nature Cell Biology' : 'ncb',
+	'Nature Chemical Biology' : 'nchembio',
+	'Nature Chemistry' : 'nchem',
+	'Nature Climate Change' : 'nclimate',
+	'Nature Communications' : 'ncomms',
+	'Nature Digest' : 'ndigest'
+	'Nature Genetics' : 'ng',
+	'Nature Geoscience' : 'ngeo',
+	'Nature Immunology' : 'ni',
+	'Nature Materials' : 'nmat',
+	'Nature Medicine' : 'nm',
+	'Nature Methods' : 'nmeth',
+	'Nature Nanotechnology' : 'nnano',
+	'Nature Neuroscience' : 'neuro',
+	'Nature Photonicsv' : 'nphoton',
+	'Nature Physics' : 'nphys',
+	'Nature Protocols' : 'nprot',
+	'Nature research journals' : ''
+	'Nature Reviews journals' : 'reviews',
+	'Nature Reviews Cancer' : 'nrc',
+	'Nature Reviews Cardiology' : 'ncardio',
+	'Nature Reviews Clinical Oncology' : 'nrclinonc',
+	'Nature Reviews Drug Discovery' : 'nrd',
+	'Nature Reviews Endocrinology' : 'nrendo',
+	'Nature Reviews Gastroenterology and Hepatology' : 'nrgastro',
+	'Nature Reviews Genetics' : 'nrg',
+	'Nature Reviews Immunology' : 'nri',
+	'Nature Reviews Microbiology' : 'nrmicro',
+	'Nature Reviews Molecular Cell Biology' : 'nrm',
+	'Nature Reviews Nephrology' : 'nrneph',
+	'Nature Reviews Neurology' : 'nrneurol',
+	'Nature Reviews Neuroscience' : 'nrm',
+	'Nature Reviews Rheumatology' : 'nrrheum',
+	'Nature Reviews Urology' : 'nrurol',
+	'Nature Structural and Molecular Biology' : 'nsmb',
+	'Neuropsychopharmacology' : 'npp',
+	'NPG Asia Materials' : 'am',
+	'Nutrition & Diabetes' : 'nutd', 
+
+	# O series
+	'Oncogene' : 'onc',
+	'Oncogenesis' : 'oncsis',
+
+	# P series
+	'Pediatric Research' : 'pr',
+	'The Pharmacogenomics Journal' : 'tpj',
+	'Polymer Journal' : 'pj',
+	'Prostate Cancer and Prostatic Diseases' : 'pcan',
+
+	# S series
+	'SciBX: Science-Business eXchange' : 'scibx',
+	'Scientific American' : 'scientificamerican',
+	'Scientific American Mind' : 'scientificamericanmind',
+	'Scientific Data' : 'scientificdata',
+	'Scientific Reports' : 'srep',
+	'Scitable Genetics' : '',
+	'Spinal Cord' : 'sc',
+
+	# T series
+	'Translational Psychiatry' : 'tp'
+}
